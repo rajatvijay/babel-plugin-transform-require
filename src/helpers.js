39e22e44864d2817153cbc53df516fa308f05c19
rajatvijay <rajatvijay5@gmail.com>
2018-04-03 12:20:32 +0530

    replaced copied refernces of matchesAst with matches from f-matches lib

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-import {isMatchWith} from 'lodash/fp';
+import {matches, extract} from "f-matches";
 
 /**
  * True when the given node is string literal.
@@ -10,114 +10,13 @@ export function isString(node) {
   return node.type === 'StringLiteral';
 }
 
-/**
- * Creates a function that matches AST against the given pattern,
- *
- * See: isAstMatch()
- *
- * @param  {Object} pattern Pattern to test against
- * @return {Function} Function that returns an object with
- * extracted fields or false when no match found.
- */
-export function matchesAst(pattern) {
-  return (ast) => isAstMatch(ast, pattern);
-}
-
-/**
- * Matches AST against the given pattern,
- *
- * Similar to LoDash.isMatch(), but with the addition that a Function
- * can be provided to assert various conditions e.g. checking that
- * number is within a certain range.
- *
- * Additionally there are utility functions:
- *
- * - extract() can be used to give names to the parts of AST -
- *   these are then returned as a map of key-value pairs.
- *
- * - matchesLength() ensures the exact array length is respected.
- *
- * @param  {Object} ast The AST node to test
- * @param {Object} pattern Pattern to test against
- * @return {Object/Boolean} an object with extracted fields
- * or false when no match found.
- */
-export function isAstMatch(ast, pattern) {
-  const extractedFields = {};
-
-  const matches = isMatchWith((value, matcher) => {
-    if (typeof matcher === 'function') {
-      const result = matcher(value);
-      if (typeof result === 'object') {
-        Object.assign(extractedFields, result);
-      }
-      return result;
-    }
-  }, pattern, ast);
-
-  if (matches) {
-    return extractedFields;
-  }
-  else {
-    return false;
-  }
-}
-
-/**
- * Utility for extracting values during matching with matchesAst()
- *
- * @param {String} fieldName The name to give for the value
- * @param {Function|Object} matcher Optional matching function or pattern for matchesAst()
- * @return {Function}
- */
-export function extract(fieldName, matcher) {
-  return (ast) => {
-    const extractedFields = {[fieldName]: ast};
-
-    if (typeof matcher === 'object') {
-      matcher = matchesAst(matcher);
-    }
-
-    if (typeof matcher === 'function') {
-      const result = matcher(ast);
-      if (typeof result === 'object') {
-        return Object.assign(extractedFields, result);
-      }
-      if (!result) {
-        return false;
-      }
-    }
-
-    return extractedFields;
-  };
-}
-
-/**
- * Utility for asserting that AST also matches the exact length
- * of the specified array pattern (in addition to matching
- * the first items in the array).
- *
- * @param {Array} pattern
- * @return {Function}
- */
-export function matchesLength(pattern) {
-  const matcher = matchesAst(pattern);
-
-  return (ast) => {
-    if (ast.length !== pattern.length) {
-      return false;
-    }
-    return matcher(ast);
-  };
-}
-
-const isIdentifier = matchesAst({
+const isIdentifier = matches({
   type: 'Identifier'
 });
 
 // matches Property with Identifier key and value (possibly shorthand)
 // TODO: Rename it to be isObjectProperty
-const isSimpleProperty = matchesAst({
+const isSimpleProperty = matches({
   type: 'ObjectProperty',
   key: isIdentifier,
   computed: false,
@@ -125,13 +24,13 @@ const isSimpleProperty = matchesAst({
 });
 
 // matches: {a, b: myB, c, ...}
-const isObjectPattern = matchesAst({
+const isObjectPattern = matches({
   type: 'ObjectPattern',
   properties: (props) => props.every(isSimpleProperty)
 });
 
 // matches: require(<source>)
-const matchRequireCall = matchesAst({
+const matchRequireCall = matches({
   type: 'CallExpression',
   callee: {
     type: 'Identifier',
@@ -145,7 +44,7 @@ const matchRequireCall = matchesAst({
 /**
  * Matches: <id> = require(<source>);
  */
-export const matchRequire = matchesAst({
+export const matchRequire = matches({
   type: 'VariableDeclarator',
   id: extract('id', id => isIdentifier(id) || isObjectPattern(id)),
   init: matchRequireCall
@@ -154,7 +53,7 @@ export const matchRequire = matchesAst({
 /**
  * Matches: <id> = require(<source>).<property>;
  */
-export const matchRequireWithProperty = matchesAst({
+export const matchRequireWithProperty = matches({
   type: 'VariableDeclarator',
   id: extract('id', isIdentifier),
   init: {
